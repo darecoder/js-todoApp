ef181739a1f0507df84f1f15934810c02914d1ba
darecoder <ektamishra1999@gmail.com>
2021-01-24 21:07:17 +0530

    updated js & css

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 //click on 'markAsDone' button
 var list = document.querySelector('ul');
 list.addEventListener('click', function(event) {
-  if (event.target.tagName === 'li') {
+  if (event.target.tagName === 'LI') {
     event.target.classList.toggle('checked');
   }
 }, false);
@@ -14,6 +14,13 @@ for (i = 0; i < close.length; i++) {
     var div = this.parentElement;
     div.style.display = "none";
   }
+}
+
+// clear the whole list
+var todoList = document.getElementById('todoList');
+function clearList() {
+  localStorage.removeItem('todoList');
+  todoList.innerHTML = '';
 }
 
 //add item to the list
@@ -29,7 +36,7 @@ function newElement() {
     }
     document.getElementById("itemInput").value = "";
   
-    var span = document.createElement("span");
+    var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
